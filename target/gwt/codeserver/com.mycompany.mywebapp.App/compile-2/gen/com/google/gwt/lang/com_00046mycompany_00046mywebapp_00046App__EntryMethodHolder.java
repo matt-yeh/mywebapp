c29c8009d9ac9f370e2690120b66196f571f36b1
mattyeh <mattyeh1982@hotmail.com>
2024-11-01 15:43:07 +0800

    first commit

---
+++ target/gwt/codeserver/com.mycompany.mywebapp.App/compile-2/gen/com/google/gwt/lang/com_00046mycompany_00046mywebapp_00046App__EntryMethodHolder.java
@@ -0,0 +1,6 @@
+package com.google.gwt.lang;
+public class com_00046mycompany_00046mywebapp_00046App__EntryMethodHolder {
+  public static final void init() {
+    // to be synthesized later
+  }
+}
